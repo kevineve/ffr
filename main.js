07c506dc4df529f0ed3ac542f6b65a512a4b380a
Kevin Eve <kevin_eve@brown.edu>
2016-12-08 15:55:18 -0500

    commit

--- main.js
+++ main.js
@@ -133,7 +133,7 @@ function storeData(foundFood){
   console.log("storeData")
   gapi.client.sheets.spreadsheets.values.append({
     "spreadsheetId": '1VZwr1nCFcEs7Cnr2u-Gq-92ayhf3QWAtlPiUdeOn7e8',
-    "range": 'Sheet1!A1:E1',
+    "range": 'Sheet1!A1:C1',
     'valueInputOption':"RAW",
     "majorDimension": "ROWS",
     "values":foundFood,
@@ -148,7 +148,7 @@ function loadContents(){
   console.log("loadContents")
   gapi.client.sheets.spreadsheets.values.get({
     "spreadsheetId": '1VZwr1nCFcEs7Cnr2u-Gq-92ayhf3QWAtlPiUdeOn7e8',
-    "range": 'Sheet1!A1:E10',
+    "range": 'Sheet1!A1:C100',
     "majorDimension": "ROWS",
   }).then(function(response) {
     //appendPre('Error: ' + response.error.message);
@@ -244,90 +244,6 @@ function getDate(){
   today = mm+'/'+dd+'/'+yyyy;
   return today
 }
-//   gapi.client.people.people.get({
-//     resourceName: 'people/me'
-//   }).then(function(resp) {
-//     var p = document.createElement('p');
-//     var name = resp.result.names[0].givenName;
-//     p.appendChild(document.createTextNode('Hello, '+name+'!'));
-//     document.getElementById('content').appendChild(p);
-//   });
-// }
- /**
- * Check if current user has authorized this application.
- */
-// function checkAuth() {
-//   gapi.auth.authorize(
-//     {
-//       'client_id': CLIENT_ID,
-//       'scope': SCOPES.join(' '),
-//       'immediate': true
-//     }, handleAuthResult);
-// }
-
-// /**
-//  * Handle response from authorization server.
-//  *
-//  * @param {Object} authResult Authorization result.
-//  */
-// function handleAuthResult(authResult) {
-//   var authorizeDiv = document.getElementById('authorize-div');
-//   if (authResult && !authResult.error) {
-//     // Hide auth UI, then load client library.
-//     authorizeDiv.style.display = 'none';
-//     loadSheetsApi();
-//   } else {
-//     // Show auth UI, allowing the user to initiate authorization by
-//     // clicking authorize button.
-//     authorizeDiv.style.display = 'inline';
-//   }
-// }
-
-// /**
-//  * Initiate auth flow in response to user clicking authorize button.
-//  *
-//  * @param {Event} event Button click event.
-//  */
-// function handleAuthClick(event) {
-//   gapi.auth.authorize(
-//     {client_id: CLIENT_ID, scope: SCOPES, immediate: false},
-//     handleAuthResult);
-//   return false;
-// }
-
-// *
-//  * Load Sheets API client library.
- 
-// function loadSheetsApi() {
-//   var discoveryUrl =
-//       'https://sheets.googleapis.com/$discovery/rest?version=v4';
-//   gapi.client.load(discoveryUrl).then(listMajors);
-// }
-
-// /**
-//  * Print the names and majors of students in a sample spreadsheet:
-//  * https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit
-//  */
-// function listMajors() {
-//   gapi.client.sheets.spreadsheets.values.get({
-//     spreadsheetId: '1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms',
-//     range: 'Class Data!A2:E',
-//   }).then(function(response) {
-//     var range = response.result;
-//     if (range.values.length > 0) {
-//       appendPre('Name, Major:');
-//       for (i = 0; i < range.values.length; i++) {
-//         var row = range.values[i];
-//         // Print columns A and E, which correspond to indices 0 and 4.
-//         appendPre(row[0] + ', ' + row[4]);
-//       }
-//     } else {
-//       appendPre('No data found.');
-//     }
-//   }, function(response) {
-//     appendPre('Error: ' + response.result.error.message);
-//   });
-// }
 
 /**
  * Append a pre element to the body containing the given message
