cb2c0ab56bdc704c1dc6ea54f3bb83faa3af3d4f
Kevin Eve <kevin_eve@brown.edu>
2016-12-08 23:15:19 -0500

    commit

--- main.js
+++ main.js
@@ -248,8 +248,9 @@ function getDate(date){
   if(date){
     var today = new Date(date);
   }
-  else:
+  else{
     var today = new Date()
+  }
   var dd = today.getDate();
   var mm = today.getMonth()+1; //January is 0!
   var yyyy = today.getFullYear();
