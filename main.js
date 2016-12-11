ac4e5e81c960cf8f86d90abb5dd513abc2009491
Kevin Eve <kevin_eve@brown.edu>
2016-12-11 14:45:08 -0500

    commit

--- main.js
+++ main.js
@@ -251,19 +251,19 @@ function getDate(date){
   else{
     var today = new Date()
   }
-  var dd = today.getDate();
-  var mm = today.getMonth()+1; //January is 0!
-  var yyyy = today.getFullYear();
+  // var dd = today.getDate();
+  // var mm = today.getMonth()+1; //January is 0!
+  // var yyyy = today.getFullYear();
 
-  if(dd<10) {
-      dd='0'+dd
-  } 
+  // if(dd<10) {
+  //     dd='0'+dd
+  // } 
 
-  if(mm<10) {
-      mm='0'+mm
-  } 
+  // if(mm<10) {
+  //     mm='0'+mm
+  // } 
 
-  today = mm+'/'+dd+'/'+yyyy;
+  // today = mm+'/'+dd+'/'+yyyy;
   return today
 }
 
