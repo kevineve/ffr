02126298d584aebf60ef4989897717decf919b59
Kevin Eve <kevin_eve@brown.edu>
2016-12-12 03:00:27 -0500

    commit

--- main.js
+++ main.js
@@ -245,7 +245,7 @@ function appendItem(src,item,date){
 
 //http://stackoverflow.com/questions/1026069/how-do-i-make-the-first-letter-of-a-string-uppercase-in-javascript
 function capitalizeFirstLetter(string) {
-    return string.charAt(0).toUpperCase() + string.slice(1);
+    return string.charAt(0) + string.slice(1).toLowerCase();
 }
 
 /**
