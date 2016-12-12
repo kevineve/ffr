f3d96e293623e7ea9e60a3d574e446a7c9f90d8f
Kevin Eve <kevin_eve@brown.edu>
2016-12-12 02:18:37 -0500

    commit

--- main.js
+++ main.js
@@ -36,7 +36,7 @@ var signoutButton = document.getElementById('signout-button');
 console.log(signoutButton)
 var submitButton = document.getElementById('submit-button');
 console.log(submitButton)
-var outputDiv = document.getElementById('autocaption');
+var outputDiv = document.createElement('div')
 console.log(outputDiv)
 console.log("yus")
 // $(function () {
